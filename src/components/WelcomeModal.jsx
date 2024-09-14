f4a5855a6bdd57cb7db46347060d4acf5960577b
Christa Favano <crstav@gmail.com>
2024-09-14 19:35:29 -0400

    Add welcome modal

---
+++ src/components/WelcomeModal.jsx
@@ -0,0 +1,24 @@
+
+
+
+
+function WelcomeModal() {
+
+    return (
+        <div>
+            <div id="welcome-modal" className="modal">
+
+
+            <div className="modal-content">
+            <span className="close-modal">&times;</span>
+            <p>Welcome!</p>
+
+            <button id='start-game' type='button'>Let's Go</button>
+            </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default WelcomeModal
